cdad2b7263ea773789a4166a63af45c3dcf8f4d5
Ninoweer <126668431+Ninoweer@users.noreply.github.com>
2026-06-15 02:18:08 +0200

    Update Get NBA Results.sql

--- MTA-Action/queries/Get NBA Results.sql
+++ MTA-Action/queries/Get NBA Results.sql
@@ -1,9 +1,16 @@
 SELECT
   current_channel,
-  most_likely_next_action,
+  next_channel,
+  most_likely_next_channel,
+  is_most_likely_next_channel,
+  next_channel_rank,
+  pct_next_channel_percent,
   transition_count,
-  ROUND(100 * pct_of_next_actions_from_current_channel, 1) AS pct_next_action,
+  journeys,
   orders,
   revenue
-FROM `action-dwh.attribution_model_ecom.mta_next_best_action_top_20260528_20260610`
-ORDER BY current_channel;
+FROM `action-dwh.attribution_model_ecom.mta_next_channel_probability_20260528_20260610`
+ORDER BY
+  current_channel,
+  next_channel_rank,
+  next_channel;
